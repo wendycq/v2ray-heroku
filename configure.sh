b5a6f9786d63e014916cd41679afa439fb2b8d94
wendycq <48820402+wendycq@users.noreply.github.com>
2020-09-13 10:10:47 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -28,7 +28,10 @@ cat << EOF > /usr/local/etc/v2ray/config.json
                 "disableInsecureEncryption": true
             },
             "streamSettings": {
-                "network": "ws"
+                "network": "ws",
+                "wsSettings": {
+                        "path": "${WSPATH}"
+                 }
             }
         }
     ],
